101430afebcd18f438df385fd99f93b0a9790304
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-11-03 16:21:15 +0700

    pacman run long way

--- core/src/com/pruek/pacmangame/GameScreen.java
+++ core/src/com/pruek/pacmangame/GameScreen.java
@@ -7,15 +7,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameScreen extends ScreenAdapter {
 	private PacmanGame pacmanGame;
 	private Texture pacmanImg;
+	private int x;
+    private int y;
 	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
+        x = 100;
+        y = 100;
     }
     public void render(float delta) {
         SpriteBatch batch = pacmanGame.batch;
         batch.begin();
         batch.draw(pacmanImg, 100, 100);
+        x += 5;
+        batch.draw(pacmanImg, x, y);
         batch.end();
     }
 }
