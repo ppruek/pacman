3f6e2622665359a7f6ae272007d3d78cacccccbc
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-11-03 16:24:05 +0700

    pacman run 4 direction by using arrow

--- core/src/com/pruek/pacmangame/GameScreen.java
+++ core/src/com/pruek/pacmangame/GameScreen.java
@@ -1,6 +1,9 @@
 package com.pruek.pacmangame;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,11 +19,26 @@ public class GameScreen extends ScreenAdapter {
         x = 100;
         y = 100;
     }
+    private void update(float delta) {
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+            y += 10;
+        }
+    }
     public void render(float delta) {
         SpriteBatch batch = pacmanGame.batch;
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        update(delta);
         batch.begin();
-        batch.draw(pacmanImg, 100, 100);
-        x += 5;
         batch.draw(pacmanImg, x, y);
         batch.end();
     }
