d3e73fdf12dce9a08c242dc1c1d1d5d869113317
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-11-03 14:21:12 +0700

    can run

---
+++ desktop/src/com/pruek/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pruek.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pruek.pacman.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
