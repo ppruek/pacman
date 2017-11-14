5501677fbe570adb76a4872d4a92e56cd5442197
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-11-14 11:03:24 +0700

    add static speed

--- core/src/com/pruek/pacmangame/Pacman.java
+++ core/src/com/pruek/pacmangame/Pacman.java
@@ -9,6 +9,7 @@ public class Pacman {
 	 public static final int DIRECTION_DOWN = 3;
 	 public static final int DIRECTION_LEFT = 4;
 	 public static final int DIRECTION_STILL = 0;
+	 public static final int SPEED = 10;
 	 private static final int [][] DIR_OFFSETS = new int [][] {
 	        {0,0},
 	        {0,-1},
@@ -26,7 +27,7 @@ public class Pacman {
 	 }
 	 
 	 public void move(int dir) { 
-		 position.x += 10 * DIR_OFFSETS[dir][0];
-	     position.y += 10 * DIR_OFFSETS[dir][1];
+		 position.x += SPEED * DIR_OFFSETS[dir][0];
+	     position.y += SPEED * DIR_OFFSETS[dir][1];
 	 }
 }
