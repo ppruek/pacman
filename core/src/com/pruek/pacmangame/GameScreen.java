52382b4df743a71e78d05c6a9771ae0e059963b8
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-11-14 11:01:05 +0700

    make direction

--- core/src/com/pruek/pacmangame/GameScreen.java
+++ core/src/com/pruek/pacmangame/GameScreen.java
@@ -6,31 +6,37 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	private PacmanGame pacmanGame;
 	private Texture pacmanImg;
-	private int x;
-    private int y;
+	//private int x,y;
+	private Pacman pacman;
 	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+        //x = 100;
+        //y = 100;
+        pacman = new Pacman(100, 100);
     }
     private void update(float delta) {
     	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+            //x -= 10;
+    		pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+            //x += 10;
+        	pacman.move(Pacman.DIRECTION_RIGHT);
         }
         if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-            y -= 10;
+            //y -= 10;
+        	pacman.move(Pacman.DIRECTION_DOWN);
         }
         if(Gdx.input.isKeyPressed(Keys.UP)) {
-            y += 10;
+            //y += 10;
+        	pacman.move(Pacman.DIRECTION_UP);
         }
     }
     public void render(float delta) {
@@ -39,7 +45,9 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         update(delta);
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
+        //batch.draw(pacmanImg, x, y);
         batch.end();
     }
 }
